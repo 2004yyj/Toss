b9c8ea8eb9fcc6b20fb20a9771216e126fddd5c3
yang-yunjae <2004yyj@naver.com>
2021-08-27 09:45:21 +0900

    Update Main Page BottomNavigationView

--- presentation/src/main/java/ks/hs/dgsw/toss/ui/view/activity/MainActivity.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/ui/view/activity/MainActivity.kt
@@ -2,11 +2,26 @@ package ks.hs.dgsw.toss.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ks.hs.dgsw.toss.R
+import ks.hs.dgsw.toss.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        navController = (supportFragmentManager
+            .findFragmentById(binding.navHostFragmentMain.id) as NavHostFragment)
+            .navController
+        binding.bnvMain.setupWithNavController(navController)
     }
 }
