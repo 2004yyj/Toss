afc62540f111ccd0ec624d37e8a1f6c9b1a70045
yang-yunjae <2004yyj@naver.com>
2021-08-30 18:33:27 +0900

    Update 송금 액티비티에 뒤로가기 버튼 추가

--- presentation/src/main/java/ks/hs/dgsw/toss/ui/view/activity/RemitActivity.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/ui/view/activity/RemitActivity.kt
@@ -2,6 +2,10 @@ package ks.hs.dgsw.toss.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import ks.hs.dgsw.toss.R
 
 class RemitActivity : AppCompatActivity() {
@@ -12,6 +16,23 @@ class RemitActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         super.onBackPressed()
-        overridePendingTransition(R.anim.pop_slide_in_left, R.anim.pop_slide_out_right)
+        finishActivity()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finishActivity()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun finishActivity() {
+        if (supportFragmentManager.backStackEntryCount == 0) {
+            finish()
+            overridePendingTransition(R.anim.pop_slide_in_left, R.anim.pop_slide_out_right)
+        }
     }
 }
