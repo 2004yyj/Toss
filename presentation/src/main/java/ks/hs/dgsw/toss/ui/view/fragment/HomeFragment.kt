29f4a9ab50980b4e2703de2153ec191ae95a6302
yang-yunjae <2004yyj@naver.com>
2021-08-30 10:41:09 +0900

    Update 홈 프래그먼트 XML 완성

--- presentation/src/main/java/ks/hs/dgsw/toss/ui/view/fragment/HomeFragment.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/ui/view/fragment/HomeFragment.kt
@@ -6,13 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import ks.hs.dgsw.toss.R
 import ks.hs.dgsw.toss.databinding.FragmentHomeBinding
+import ks.hs.dgsw.toss.ui.view.util.EventObserver
 import ks.hs.dgsw.toss.ui.viewmodel.factory.HomeViewModelFactory
 import ks.hs.dgsw.toss.ui.viewmodel.fragment.HomeViewModel
 
 class HomeFragment : Fragment() {
 
+    private val navController by lazy { findNavController() }
     private lateinit var binding: FragmentHomeBinding
     private lateinit var viewModel: HomeViewModel
 
@@ -23,6 +26,7 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater)
         viewModel = ViewModelProvider(this, HomeViewModelFactory())[HomeViewModel::class.java]
         binding.vm = viewModel
+        binding.lifecycleOwner = this
         return binding.root
     }
 
@@ -34,5 +38,8 @@ class HomeFragment : Fragment() {
 
     private fun init() {
         viewModel.getAccounts()
+        viewModel.openRemitPage.observe(viewLifecycleOwner, EventObserver {
+            // 결제 페이지로 넘어가기
+        })
     }
 }
--- presentation/src/main/java/ks/hs/dgsw/toss/ui/viewmodel/fragment/HomeViewModel.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/ui/viewmodel/fragment/HomeViewModel.kt
@@ -1,17 +1,28 @@
 package ks.hs.dgsw.toss.ui.viewmodel.fragment
 
 import androidx.databinding.ObservableArrayList
-import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ks.hs.dgsw.domain.entity.Account
+import ks.hs.dgsw.toss.ui.view.util.Event
 
 class HomeViewModel(): ViewModel() {
     val accountList = ObservableArrayList<Account>()
+
+    private val _openRemitPage = MutableLiveData<Event<String>>()
+    val openRemitPage = _openRemitPage
+
+    fun getUserInfo() {
+
+    }
 
     fun getAccounts() {
         accountList.clear()
         accountList.addAll(arrayListOf(Account("대구은행", "20513158980", "양윤재", 5000)))
     }
+
+    fun remitPage() {
+        _openRemitPage.value = Event("openRemitPage")
+    }
+
 }
