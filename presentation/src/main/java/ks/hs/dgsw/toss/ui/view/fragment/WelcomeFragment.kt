c0700f07c7dcfb0a36bdbd992fb91869277a56bf
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-09 20:56:04 +0900

    Update WelcomeFragment->SignUpFirstFragment 연결

--- presentation/src/main/java/ks/hs/dgsw/toss/ui/view/fragment/WelcomeFragment.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/ui/view/fragment/WelcomeFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import ks.hs.dgsw.toss.R
+import ks.hs.dgsw.toss.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private val navController: NavController by lazy { findNavController() }
+    private lateinit var binding: FragmentWelcomeBinding
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignUp: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        binding = FragmentWelcomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        btnLogin = binding.btnLoginWelcome
+        btnSignUp = binding.btnSignUpWelcome
+
+        btnLogin.setOnClickListener { navController.navigate(R.id.action_welcomeFragment_to_loginFragment) }
+        btnSignUp.setOnClickListener { navController.navigate(R.id.action_welcomeFragment_to_signUpFragment) }
     }
 
 }
