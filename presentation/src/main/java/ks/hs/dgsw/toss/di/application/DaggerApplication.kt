9a20de58c129026be777b5963bde7725567724a4
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-10 20:55:24 +0900

    Update SharedPreference 초기화 코드 application에 추가

--- presentation/src/main/java/ks/hs/dgsw/toss/di/application/DaggerApplication.kt
+++ presentation/src/main/java/ks/hs/dgsw/toss/di/application/DaggerApplication.kt
@@ -2,6 +2,12 @@ package ks.hs.dgsw.toss.di.application
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import ks.hs.dgsw.toss.ui.view.util.PreferenceHelper
 
 @HiltAndroidApp
-class DaggerApplication: Application()
+class DaggerApplication: Application() {
+    override fun onCreate() {
+        PreferenceHelper.init(applicationContext)
+        super.onCreate()
+    }
+}
